84fa1cc15d8ec4ced8db672b0b4f1733566b2244
VarunGuptaPy <varun.gupta.py@gmail.com>
2024-10-02 10:39:36 +0530

    added env

--- src/firebase.js
+++ src/firebase.js
@@ -8,7 +8,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBv1RkSAND5h9gnsMxQpbrLb9HUEbio0gY",
+  apiKey: import.meta.env.VITE_FIREBASE_API,
   authDomain: "helping-ai.firebaseapp.com",
   projectId: "helping-ai",
   storageBucket: "helping-ai.appspot.com",
