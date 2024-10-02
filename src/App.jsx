83ab1fffdbd8a9da2aebfc35197b0785ff2c0b28
VarunGuptaPy <varun.gupta.py@gmail.com>
2024-10-02 10:24:25 +0530

    added icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import HoverButton from "./widgets/HoverButton";
 import FAQ from "./widgets/FAQ";
